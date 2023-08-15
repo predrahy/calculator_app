663ad170012e5f46141cfed766953e4eb8f26c61
keneilwe thobega <thobegak@gmail.com>
2023-08-15 15:40:09 +0200

    some changes

--- index.js
+++ index.js
@@ -7,12 +7,37 @@ const display = (value) => {
 };
 
 let ans = [];
+// let nums;
+
+const add = () => {
+  let q = document.getElementById("result").value;
+  if (q.includes("+")) {
+    ans = q.split("+");
+    console.log(+ans[0] + +ans[1]);
+  }
+};
+
+const subtract = () => {
+  let q = document.getElementById("result").value;
+  if (q.includes("-")) {
+    ans = q.split("-");
+    console.log(+ans[0] - +ans[1]);
+  }
+};
 
 const multiply = () => {
   let q = document.getElementById("result").value;
   if (q.includes("*")) {
     ans = q.split("*");
-    return +ans[0] * +ans[1];
+    // const nums = ans.map((str) => {
+    //   return +str;
+    // });
+    // console.log(nums.reduce((a, b) => a * b));
+    // console.log(nums);
+    // console.log(ans);
+    // console.log(nums);
+    console.log(+ans[0] * +ans[1]);
+    // return +ans[0] * +ans[1];
   }
 };
 
@@ -20,8 +45,17 @@ const divide = () => {
   let q = document.getElementById("result").value;
   if (q.includes("/")) {
     ans = q.split("/");
-    return +ans[0] / +ans[1];
+    console.log(+ans[0] / +ans[1]);
   }
+};
+
+const percent = () => {
+  let q = document.getElementById("result").value;
+  console.log(q / 100);
+};
+
+const backSpace = () => {
+  document.getElementById("result").value.substr(0, -1);
 };
 
 const calculate = () => {
@@ -29,6 +63,11 @@ const calculate = () => {
 };
 
 const operate = () => {
+  add();
+  subtract();
   multiply();
   divide();
+  percent();
+  backSpace();
+  return;
 };
