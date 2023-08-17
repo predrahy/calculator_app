c2fcdba0a0312e824c6709cc93b9c4da4ad96e18
keneilwe thobega <thobegak@gmail.com>
2023-08-17 09:45:15 +0200

    all operators working

--- index.js
+++ index.js
@@ -1,50 +1,43 @@
+let output = document.getElementById("result");
+
 const clearScreen = () => {
-  document.getElementById("result").value = "";
+  output.value = "";
 };
 
 const display = (value) => {
-  document.getElementById("result").value += value;
+  output.value += value;
 };
 
 const backSpace = () => {
-  let q = document.getElementById("result").value;
-  q = q.slice(0, -1);
+  output.value = output.value.slice(0, -1);
 };
 
 let ans = [];
 
 const add = () => {
-  let q = document.getElementById("result").value;
-
-  if (q.includes("+")) {
-    ans = q.split("+");
+  if (output.value.includes("+")) {
+    ans = output.value.split("+");
     return +ans[0] + +ans[1];
   }
 };
 
 const subtract = () => {
-  let q = document.getElementById("result").value;
-
-  if (q.includes("-")) {
-    ans = q.split("-");
+  if (output.value.includes("-")) {
+    ans = output.value.split("-");
     return +ans[0] - +ans[1];
   }
 };
 
 const multiply = () => {
-  let q = document.getElementById("result").value;
-
-  if (q.includes("*")) {
-    ans = q.split("*");
+  if (output.value.includes("*")) {
+    ans = output.value.split("*");
     return +ans[0] * +ans[1];
   }
 };
 
 const divide = () => {
-  let q = document.getElementById("result").value;
-
-  if (q.includes("/")) {
-    ans = q.split("/");
+  if (output.value.includes("/")) {
+    ans = output.value.split("/");
     if (+ans[1] === 0) {
       return "Can't div by 0";
     }
@@ -53,14 +46,14 @@ const divide = () => {
 };
 
 const percent = () => {
-  let q = document.getElementById("result").value;
-  if (q) return +q / 100;
+  if (output.value) {
+    return (output.value = +output.value / 100);
+  }
 };
 
 //disable decimal if one already exists
 const disableDecimal = () => {
-  let q = document.getElementById("result").value;
-  if (q.includes(".")) {
+  if (output.value.includes(".")) {
     document.getElementById("decimal-btn").disabled = true;
   } else {
     document.getElementById("decimal-btn").disabled = false;
@@ -69,14 +62,13 @@ const disableDecimal = () => {
 
 //keyboard support
 const keySupport = () => {
-  let keyInput = document.getElementById("keyboard").value;
   document.addEventListener("keydown", (event) => {
-    keyInput += event.key;
+    output.value += event.key;
   });
 };
 
 const calculate = () => {
-  document.getElementById("result").value = operate();
+  output.value = operate();
 };
 
 const operate = () => {
@@ -88,6 +80,6 @@ const operate = () => {
     backSpace() ||
     percent() ||
     keySupport() ||
-    dot()
+    disableDecimal()
   );
 };
