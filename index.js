7815acda4a4edabc6a4b62db2a85527afa0a59c3
raisatarase <raisa.tarase@gmail.com>
2023-08-18 19:49:54 +0300

    operations chaining done

--- index.js
+++ index.js
@@ -1,23 +1,128 @@
 let output = document.getElementById("result");
+let inputArray = [];
 
-let operators = ["*", "+", "-", "%", "/"];
+// assigned precendence to operators
+let operators = {
+  "*": 2,
+  "/": 2,
+  "+": 1,
+  "-": 1,
+  "%": 2,
+};
 
 const clearScreen = () => {
   output.value = "";
+  inputArray = [];
 };
 
+// new display function code
 const display = (value) => {
-  if (output.value.includes(value)) {
-    if (!operators.includes(value))
+  const lastChar = output.value.slice(-1);
+
+  if (value === "." && output.value.includes(".")) {
+    return;
+  }
+
+  if (!operators.hasOwnProperty(value)) {
+    if (output.value === "0" || operators.hasOwnProperty(lastChar)) {
+      output.value = value;
+    } else {
       output.value += value;
+    }
+    inputArray.push(value);
   } else {
-    output.value += value;
+    if (!operators.hasOwnProperty(lastChar)) {
+      output.value += value;
+      inputArray.push(value);
+    } else if (value === "-" && lastChar !== "-") {
+      output.value += value;
+      inputArray.push(value);
+    } else if (lastChar !== "-" && lastChar !== "=") {
+      output.value = output.value.slice(0, -1) + value;
+      inputArray[inputArray.length - 1] = value;
+    }
   }
 };
+//display function code finished
 
 const backSpace = () => {
   output.value = output.value.slice(0, -1);
+  inputArray.pop();
 };
+
+// code to evaluate expression
+const evaluateExpression = () => {
+  let operandStack = [];
+  let operatorStack = [];
+
+  let currentOperand = "";
+
+  for (let i = 0; i < inputArray.length; i++) {
+    let token = inputArray[i];
+
+    if (!operators.hasOwnProperty(token)) {
+      currentOperand += token;
+    } else {
+      operandStack.push(parseFloat(currentOperand));
+      currentOperand = "";
+
+      while (
+        operatorStack.length > 0 &&
+        operators[operatorStack[operatorStack.length - 1]] >= operators[token]
+      ) {
+        let operator = operatorStack.pop();
+        let operand2 = operandStack.pop();
+        let operand1 = operandStack.pop();
+
+        operandStack.push(performOperation(operand1, operand2, operator));
+      }
+
+      operatorStack.push(token);
+    }
+  }
+
+  operandStack.push(parseFloat(currentOperand));
+
+  while (operatorStack.length > 0) {
+    let operator = operatorStack.pop();
+    let operand2 = operandStack.pop();
+    let operand1 = operandStack.pop();
+
+    operandStack.push(performOperation(operand1, operand2, operator));
+  }
+
+  if (operandStack.length === 1) {
+    output.value = operandStack[0];
+    inputArray = [operandStack[0]];
+  } else {
+    output.value = "Error";
+    inputArray = [];
+  }
+};
+// code to evaluate expression finished
+
+// code to perform operation
+const performOperation = (operand1, operand2, operator) => {
+  switch (operator) {
+    case "+":
+      return operand1 + operand2;
+    case "-":
+      return operand1 - operand2;
+    case "*":
+      return operand1 * operand2;
+    case "/":
+      if (operand2 === 0) {
+        output.value = "Can't div by 0";
+        throw new Error("Division by zero");
+      }
+      return operand1 / operand2;
+    case "%":
+      return operand1 % operand2;
+    default:
+      throw new Error("Invalid operator");
+  }
+};
+// code to perform operation  end here
 
 let ans = [];
 
@@ -84,7 +189,8 @@ const keySupport = () => {
 };
 
 const calculate = () => {
-  output.value = operate();
+  // output.value = operate();
+  evaluateExpression();
 };
 
 const operate = () => {
